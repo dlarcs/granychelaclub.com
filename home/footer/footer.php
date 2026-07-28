eb0046b9c8d7a61792b6e19d94276b6418d21e36
Laura  Rozo <admin@promoflow.net>
2026-07-28 10:08:31 -0500

    “…”

--- home/footer/footer.php
+++ home/footer/footer.php
@@ -8,7 +8,7 @@ $cssVer = is_file($cssFile) ? filemtime($cssFile) : '';
   <div class="footer-inner">
 
     <div class="footer-brand">
-      <h1>Gran&Chela Club</h1>
+      <h2>Gran&Chela Club</h2>
       <p>Bar, club, discoteca y eventos</p>
 
       <div class="footer-social">
